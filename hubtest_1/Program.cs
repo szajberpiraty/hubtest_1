dfaff85ac1a3608f55f0a82407c6ba6bd5a36dac
szajberpiraty <szajberpiraty@gmail.com>
2018-07-22 20:33:01 +0200

    Deklarációk

--- hubtest_1/Program.cs
+++ hubtest_1/Program.cs
@@ -8,13 +8,21 @@ namespace hubtest_1
 {
     class Program
     {
+        
+
         static void Main(string[] args)
         {
+            var aValtozo = 20;
+            int bValtozo = 40;
+
+            var tomb= new int[6];
+            int[] tomb2 = new int[6];
+
             Console.WriteLine("Hello Git!");
             Console.ReadKey();
             Console.WriteLine("Hello Git again!");
             Console.ReadKey();
-            Console.WriteLine("Hello Gitter !");
+            Console.WriteLine("Hello Gitter !{0},{1}",aValtozo,bValtozo);
             Console.ReadKey();
         }
     }
